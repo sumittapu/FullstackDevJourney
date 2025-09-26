567c5c7b3c9265fab2ea4e5a05ac80fa035fa51d
sumit_tapu <greattapu2016@gmail.com>
2025-09-26 16:56:34 +0530

    Day 2: Variables & Data Types

---
+++ semester1/day02-variables/variables.js
@@ -0,0 +1,22 @@
+let name = "Tapu";
+let age = 22;
+let height = 6.5;
+let isMarried = false;
+let country = "India";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Height:", height);
+console.log("Married?", isMarried);
+console.log("Country:", country);
+
+let x = 10;
+let y = 3;
+
+console.log("Add:", x+y);
+console.log("Substract:", x-y);
+console.log("Multiply", x*y);
+console.log("Divide", x/y);
+
+let myAge = 25;
+console.log("Age is " + myAge);
